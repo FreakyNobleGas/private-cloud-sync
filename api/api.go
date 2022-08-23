663e7272be646978199f98be6877f66c622a3609
Nick Quinn <31631401+FreakyNobleGas@users.noreply.github.com>
2022-08-23 13:39:34 -0500

    Added additional layer in JSON file to have keys represent top level parent directory.

--- api/api.go
+++ api/api.go
@@ -10,34 +10,41 @@ import (
 	"path/filepath"
 )
 
+// FilesJSON Holds all file information to be converted to JSON
 type FilesJSON struct {
-	Files []string
+	Files map[string][]string
 }
 
-func getAllFiles() (allFiles []string, err error) {
+func getAllFiles(allFiles *map[string][]string) (map[string][]string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal("ERROR: Can't get home directory path.")
 	}
 
-	getFolders := []string{"Documents"}
+	getFolders := []string{"Documents", "Pictures", "Videos"}
 	for _, f := range getFolders {
 		nextDir := homeDir + "\\" + f
+		(*allFiles)[nextDir] = make([]string, 0)
+		var nextDirMapping = (*allFiles)[nextDir]
 		err = filepath.Walk(nextDir, func(path string, info fs.FileInfo, err error) error {
 			if err != nil {
 				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 				return err
 			}
-			allFiles = append(allFiles, path)
+			nextDirMapping = append(nextDirMapping, path)
 			return nil
 		})
+		(*allFiles)[nextDir] = nextDirMapping
 		if err != nil {
 			fmt.Printf("error walking the path: %v\n", err)
-			return allFiles, err
+			return *allFiles, err
 		}
 	}
-	log.Printf("INFO: Retrieved %v files in user directory.", len(allFiles))
-	return allFiles, err
+	for k, v := range *allFiles {
+		log.Printf("INFO: Retrieved %v files in %v.", len(v), k)
+	}
+
+	return *allFiles, err
 }
 
 func enableCors(w *http.ResponseWriter) {
@@ -47,24 +54,28 @@ func enableCors(w *http.ResponseWriter) {
 
 func httpGetAllFiles(w http.ResponseWriter, _ *http.Request) {
 	enableCors(&w)
-	files, err := getAllFiles()
+	var allFiles = make(map[string][]string)
+	files, err := getAllFiles(&allFiles)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	allFiles, err := json.Marshal(&FilesJSON{
+	allFilesJSON, err := json.Marshal(&FilesJSON{
 		files,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	w.Write(allFiles)
+	w.Write(allFilesJSON)
 }
 
 func main() {
-	getAllFiles()
-
+	var allFiles = make(map[string][]string)
+	t, _ := getAllFiles(&allFiles)
+	for k, _ := range t {
+		log.Println(k)
+	}
 	log.Println("INFO: Serving HTTP Service")
 	http.HandleFunc("/getAllFiles", httpGetAllFiles)
 	err := http.ListenAndServe(":5000", nil)
