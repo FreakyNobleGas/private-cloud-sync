a2aa19bddf0722bccb7c2669e81ec14d1d5cf3b8
Nick Quinn <31631401+FreakyNobleGas@users.noreply.github.com>
2022-08-23 19:04:15 -0500

    All folders now have their own key in the JSON data structure, with the files specific to their directory as the value.

--- api/api.go
+++ api/api.go
@@ -8,43 +8,81 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // FilesJSON Holds all file information to be converted to JSON
 type FilesJSON struct {
-	Files map[string][]string
+	Files map[string]interface{}
 }
 
-func getAllFiles(allFiles *map[string][]string) (map[string][]string, error) {
+func getFiles(nextDir string, allFiles map[string]interface{}) (map[string]interface{}, error) {
+	if allFiles[nextDir] == nil {
+		allFiles[nextDir] = make([]string, 0)
+
+	}
+	var nextDirMapping = allFiles[nextDir].([]string)
+
+	err := filepath.Walk(nextDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if info.IsDir() {
+			if strings.Compare(nextDir, path) != 0 {
+				allFiles[path] = make(map[string]interface{})
+				getFiles(path, allFiles[path].(map[string]interface{}))
+			}
+		} else {
+			nextDirMapping = append(nextDirMapping, path)
+		}
+
+		return nil
+	})
+
+	allFiles[nextDir] = nextDirMapping
+	if err != nil {
+		fmt.Printf("error walking the path: %v\n", err)
+	}
+
+	return allFiles, err
+}
+
+func getAllFiles() (map[string]interface{}, error) {
+	var allFiles = make(map[string]interface{})
+
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal("ERROR: Can't get home directory path.")
 	}
+	// TODO: Remove Test Dir
+	homeDir = "C:\\Users\\nickq\\Desktop\\testing"
 
 	getFolders := []string{"Documents", "Pictures", "Videos"}
+	getFolders = []string{"A", "B", "C"}
 	for _, f := range getFolders {
 		nextDir := homeDir + "\\" + f
-		(*allFiles)[nextDir] = make([]string, 0)
-		var nextDirMapping = (*allFiles)[nextDir]
-		err = filepath.Walk(nextDir, func(path string, info fs.FileInfo, err error) error {
-			if err != nil {
-				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-				return err
-			}
-			nextDirMapping = append(nextDirMapping, path)
-			return nil
-		})
-		(*allFiles)[nextDir] = nextDirMapping
-		if err != nil {
-			fmt.Printf("error walking the path: %v\n", err)
-			return *allFiles, err
-		}
-	}
-	for k, v := range *allFiles {
-		log.Printf("INFO: Retrieved %v files in %v.", len(v), k)
+		allFiles[nextDir] = make([]string, 0)
+		allFiles, err = getFiles(nextDir, allFiles)
 	}
 
-	return *allFiles, err
+	var printer func(allFiles map[string]interface{})
+	printer = func(allFiles map[string]interface{}) {
+		for k, v := range allFiles {
+			switch v := v.(type) {
+			case map[string]interface{}:
+				printer(v)
+			case []string:
+				fmt.Printf("Retrieved %v files in %v.\n", len(v), k)
+			default:
+				fmt.Printf("I don't know about type %T!\n", v)
+			}
+		}
+	}
+	printer(allFiles)
+
+	return allFiles, err
 }
 
 func enableCors(w *http.ResponseWriter) {
@@ -54,8 +92,7 @@ func enableCors(w *http.ResponseWriter) {
 
 func httpGetAllFiles(w http.ResponseWriter, _ *http.Request) {
 	enableCors(&w)
-	var allFiles = make(map[string][]string)
-	files, err := getAllFiles(&allFiles)
+	files, err := getAllFiles()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,11 +108,8 @@ func httpGetAllFiles(w http.ResponseWriter, _ *http.Request) {
 }
 
 func main() {
-	var allFiles = make(map[string][]string)
-	t, _ := getAllFiles(&allFiles)
-	for k, _ := range t {
-		log.Println(k)
-	}
+	log.Println("INFO: Testing getAllFiles()")
+	_, _ = getAllFiles()
 	log.Println("INFO: Serving HTTP Service")
 	http.HandleFunc("/getAllFiles", httpGetAllFiles)
 	err := http.ListenAndServe(":5000", nil)
